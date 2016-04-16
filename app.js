4c0a290f642c7a9828549d8e4094c13e02128e89
Jayanth M S <jayanthms7@gmail.com>
2016-04-16 13:27:46 +0530

    Git commands and message format done - WIP on Slack side
    - only sends the message if there is a diff change in a file between
      commits

---
+++ app.js
@@ -0,0 +1,42 @@
+var app     = require('express')();
+var gitexec = require('gitexec');
+var config  = require('./config');
+var request = require('request');
+
+app.post('/notify-slack', function (req, res) {
+  // handle errors that will be in return Stream object
+  gitexec.exec(config.repoPath, config.gitFetchCommand);
+  gitexec.exec(config.repoPath, config.gitRebaseCommand);
+
+  gitexec.execCollect(config.repoPath, config.gitDiffCommand, function(err, response) {
+    if (err) {
+      // handle this error and bail
+    }
+
+    if (response) {
+      var requestData = {
+        text: config.warningHeader + '```' + response + '```'
+      }
+
+      request({
+        url: config.slackWebhookUrl,
+        method: 'POST',
+        json: requestData
+
+      }, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+          console.log(body)
+        } else {
+          console.log("error: " + error)
+          console.log("response.statusCode: " + response.statusCode)
+          console.log("response.statusText: " + response.statusText)
+        }
+
+      });
+    }
+  });
+});
+
+app.listen(config.port, function () {
+  console.log('Server listening on port %d', config.port);
+});
---
+++ config-example.js
@@ -0,0 +1,18 @@
+var config = {}
+
+config.repoPath         = "";
+config.watchingBranch   = "";
+config.remoteName       = "origin";
+config.watchingFilePath = repoPath + "";
+config.slackApiToken    = "";
+
+config.gitFetchCommand  = "git fetch";
+config.gitRebaseCommand = "git rebase " + config.remoteName + config.watchingBranch;
+config.gitDiffCommand   = "git diff HEAD HEAD^ " + watchingFilePath;
+
+config.port = 3000;
+
+config.warningHeader = ":fire: :red_circle: :point_right: *README changed* :point_left: :red_circle: :fire:\n"
+
+
+module.exports = config;
